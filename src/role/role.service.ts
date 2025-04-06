eee66f74e7e835723e8b4423a6bcad2acaba7aef
Mahdi sadeghi <mahdi.sadeghi1915@gmail.com>
2025-04-06 21:06:30 +0330

    Create Role

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { Role } from './entities/role.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class RoleService {
+
+  constructor(
+    @InjectRepository(Role)
+    private roleRepository : Repository<Role>,
+  ){}
+
   create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+    const role = new Role();
+    Object.assign(role,createRoleDto)
+    return this.roleRepository.save(role);
   }
 
   findAll() {
