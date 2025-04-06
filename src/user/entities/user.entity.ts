201ef4418d35658987a20c121f04369fed8a953e
Mahdi sadeghi <mahdi.sadeghi1915@gmail.com>
2025-04-06 20:07:11 +0330

    Create Role Module And Relation Between User And Role

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,5 +1,6 @@
 
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Role } from 'src/role/entities/role.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 
 @Entity()
 export class User {
@@ -21,4 +22,8 @@ export class User {
 
   @Column({ default: true })
   isActive: boolean;
+
+  @ManyToOne(() =>Role , (role) => role.users)
+  role: Role;
+
 }
