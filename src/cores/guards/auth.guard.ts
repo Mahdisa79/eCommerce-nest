59553984737044a78913a45332488e307275ca7c
Mahdi sadeghi <mahdi.sadeghi1915@gmail.com>
2025-04-06 19:11:58 +0330

    Get Current User With Auth Guard And Current User Decorator

---
+++ src/cores/guards/auth.guard.ts
@@ -0,0 +1,43 @@
+
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { UserPayload } from 'src/user/interfaces/user-payload.interface';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(private jwtService : JwtService , private configService : ConfigService){}
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean>  {
+    const request = context.switchToHttp().getRequest() as Request;
+
+    //Extract JWT bearer token header
+    const token = request.headers?.authorization?.split(' ')[1];
+    // console.log(request.headers?.authorization , token);
+    if(!token) throw new UnauthorizedException();
+
+    //Verify JWT => Payload
+
+    try {
+        
+        const currentUser = await this.jwtService.verifyAsync(token ,{secret:this.configService.get<string>('JWT_SECRET_KEY')}) as UserPayload;
+        // console.log('check current user',currentUser);
+        request.currentUser = {
+            id : currentUser.id ,
+            email : currentUser.email ,
+            firstName:currentUser.firstName ,
+            lastName:currentUser.lastName ,
+            isActive : currentUser.isActive
+        };
+        
+        return true;
+
+    } catch {
+        if(!token) throw new UnauthorizedException();
+    }
+
+
+}
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from 'src/cores/guards/auth.guard';
+import { Request } from 'express';
+import { CurrentUser } from 'src/cores/decorators/current-user.decorator';
+import { UserPayload } from './interfaces/user-payload.interface';
 
  @Controller('api/v1/users')
 export class UserController {
@@ -10,6 +14,13 @@ export class UserController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @Get('/me')
+  @UseGuards(AuthGuard)
+  getCurrentUser(@CurrentUser() user : UserPayload) {
+
+    return user;
   }
 
   @Get()
