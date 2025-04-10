5e1107fe81bf9942c27acba37e5f39f67c0caf15
Mahdi sadeghi <mahdi.sadeghi1915@gmail.com>
2025-04-10 12:52:20 +0330

    Change Permission Entity

--- src/permissions/entities/permission.entity.ts
+++ src/permissions/entities/permission.entity.ts
@@ -1,20 +1,27 @@
 import { Endpoint } from './../../endpoint/entities/endpoint.entity';
 import { Role } from "src/role/entities/role.entity";
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Permission {
-    @PrimaryGeneratedColumn()
-    id : number;
+
+    @PrimaryColumn()
+    roleName:string;
+
+    @PrimaryColumn()
+    endpointId :number;
 
     @ManyToOne(()=>Role , (role) =>role.permissions )
+    @JoinColumn({name:'roleName'})
     role : Role;
 
-    
+
     @ManyToOne(()=>Role , (role) =>role.permissions )
+    @JoinColumn({name:'endpointId'})
     endpoint : Endpoint;
 
 
+    @Column()
     isAllow:boolean;
 
 }
