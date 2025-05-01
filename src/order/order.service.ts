2e714ec213467ef589856b2c4cc9145514d7d7ef
Mahdi sadeghi <mahdi.sadeghi1915@gmail.com>
2025-05-01 03:35:23 +0330

    Calculate total order price and apply transaction

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,9 +1,9 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, ParseFloatPipe } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { OrderDetail } from './entities/order-detail.entity';
 import { CartService } from 'src/cart/cart.service';
 import { UserPayload } from 'src/user/interfaces/user-payload.interface';
@@ -20,46 +20,82 @@ export class OrderService {
     private cartService : CartService,
     private shippingAddressService : ShippingAddressService,
     private shippingRuleService : ShippingRuleService,
+    private dataSource : DataSource,
+
   ){}
 
   async process(currentUser:UserPayload , processOrderDto : ProcessOrderDto) {
-    //Shipping Address 
-    const shippingAddress = await this.shippingAddressService.findOne(processOrderDto.shippingAddressId)
 
-    if(shippingAddress.user.id !== currentUser.id)
-      throw new BadRequestException('Please use another address')
+    const queryRunner = this.dataSource.createQueryRunner();
 
-    const shippingAddressInfo = {
-      id : shippingAddress.id ,
-      value : shippingAddress.value ,
-      phoneNumber : shippingAddress.phoneNumber ,
+    try{
+
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+  
+      //Shipping Address 
+      const shippingAddress = await this.shippingAddressService.findOne(processOrderDto.shippingAddressId)
+  
+      if(shippingAddress.user.id !== currentUser.id)
+        throw new BadRequestException('Please use another address')
+  
+      const shippingAddressInfo = {
+        id : shippingAddress.id ,
+        value : shippingAddress.value ,
+        phoneNumber : shippingAddress.phoneNumber ,
+      }
+      //Shipping Rule
+      const shippingRule = await this.shippingRuleService.findOne(processOrderDto.shippingRuleId);
+      const cart = await this.cartService.findCart(currentUser.id);
+      const order = new Order();
+  
+      order.shippingAddress =  JSON.stringify(shippingAddressInfo);
+      order.shippingMethod =  JSON.stringify(shippingRule);
+      
+      // const newOrder = await this.orderRepository.save(order);
+      const newOrder = await queryRunner.manager.save(order);
+
+  
+      for(const cartItem of cart.items){
+        const newOrderDetails = new OrderDetail;
+        newOrderDetails.order = newOrder;
+        newOrderDetails.product = cartItem.product;
+        newOrderDetails.productName = cartItem.product.name;
+        newOrderDetails.productPrice = cartItem.product.price;
+        newOrderDetails.quantity = cartItem.quantity;
+        newOrderDetails.variant = cartItem.variant;
+        newOrderDetails.totalPrice = cartItem.totalPrice;
+  
+        // await this.orderDetailRepository.save(newOrderDetails)
+        await queryRunner.manager.save(newOrderDetails);
+
+      }
+  
+  
+      //remove cart items
+      // const updateOrder = await this.orderRepository.findOne({where:{id:newOrder.id},relations:{orderDetails:true}})
+      const updateOrder = await queryRunner.manager.findOne(Order,{where:{id:newOrder.id},relations:{orderDetails:true}})
+     
+      const totalOrderPrice = updateOrder?.orderDetails?.reduce((acc,curr)=>acc+parseFloat(`${curr.totalPrice}`),0)
+      updateOrder.totalPrice = totalOrderPrice;
+      // await this.orderRepository.save(updateOrder)
+      await queryRunner.manager.save(updateOrder);
+  
+      //calc total price of order
+
+      await queryRunner.commitTransaction()
+      
+
+    }catch(error){
+      console.log('check error',error);
+      
+      await queryRunner.rollbackTransaction();
+      throw new BadRequestException('Something not expected');
+    }finally{
+      await queryRunner.release();
+
     }
-    //Shipping Rule
-    const shippingRule = await this.shippingRuleService.findOne(processOrderDto.shippingRuleId);
-    const cart = await this.cartService.findCart(currentUser.id);
-    const order = new Order();
 
-    order.shippingAddress =  JSON.stringify(shippingAddressInfo);
-    order.shippingMethod =  JSON.stringify(shippingRule);
-    
-    const newOrder = await this.orderRepository.save(order);
-
-    for(const cartItem of cart.items){
-      const newOrderDetails = new OrderDetail;
-      newOrderDetails.order = newOrder;
-      newOrderDetails.product = cartItem.product;
-      newOrderDetails.productName = cartItem.product.name;
-      newOrderDetails.productPrice = cartItem.product.price;
-      newOrderDetails.quantity = cartItem.quantity;
-      newOrderDetails.variant = cartItem.variant;
-      newOrderDetails.totalPrice = cartItem.totalPrice;
-
-      await this.orderDetailRepository.save(newOrderDetails)
-    }
-
-
-    //remove cart items
-    //calc total price of order
 
     
   }
